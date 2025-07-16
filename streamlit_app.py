aac3a54c6ec0627a11f112c311acc790d0fa09ee
ganesh-ns <ganesh.kumar@powerschool.com>
2025-07-16 11:19:58 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,7 +21,7 @@ if ingredients_list:
     ingredients_string = ''
     for fruit_chosen in ingredients_list:
         ingredients_string+= fruit_chosen + ' '
-        smoothiefroot_response = requests.get("https://FRUITYVICE.com/api/fruit/watermelon")
+        smoothiefroot_response = requests.get("https://FRUITYVICE.com/api/fruit/"+fruit_chosen)
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
             values ('""" + ingredients_string + """','"""+name_on_order+"""')"""
